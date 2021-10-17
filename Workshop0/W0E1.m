b2fa7fcc5c306313d02f5a05955e52ce67728711
Sooltynk <82092254+Sooltynk@users.noreply.github.com>
2021-10-17 12:01:55 +0200

    Update W0E1.m

--- Workshop0/W0E1.m
+++ Workshop0/W0E1.m
@@ -3,14 +3,14 @@
 A = [0, -1; 2, 1];
 B = [1, 2; 3, 4];
 
-# The equation 2X+4A=3*B*A once the X is cleared, the result is 
-# X = 1/2*(3*B*A-4*A) ==> X = 0.5*(3*B*A-4*A).
+%{ The equation 2X+4A=3*B*A once the X is cleared, the result is 
+ X = 1/2*(3*B*A-4*A) ==> X = 0.5*(3*B*A-4*A). %}
 
 X = 0.5*(3*B*A-4*A);
 disp("Solution matrix")
 disp(X)
 
-# Check if the solution of the matrix equation 2X+4A=3*B*A is correct. 
+% Check if the solution of the matrix equation 2X+4A=3*B*A is correct. 
 
 left_matrix = 2*X+4*A;
 right_matrix = 3*B*A;
