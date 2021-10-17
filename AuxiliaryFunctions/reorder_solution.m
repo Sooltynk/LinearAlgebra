49e30af32fde0b770ed82f25f0c7a3be2d833718
Sooltynk <82092254+Sooltynk@users.noreply.github.com>
2021-10-18 00:55:52 +0200

    Create reorder_solution.m

---
+++ AuxiliaryFunctions/reorder_solution.m
@@ -0,0 +1,7 @@
+function reordered_solution = reorder_solution(solution, permutation)
+  n = length(solution);
+  reordered_solution = zeros(size(solution));
+  for k = 1:n
+    reordered_solution(permutation(k)) = solution(k);
+  endfor
+endfunction
