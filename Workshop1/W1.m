549fa074ead4571cba003c9631bb7b13618a1b62
Sooltynk <82092254+Sooltynk@users.noreply.github.com>
2021-10-17 23:39:59 +0200

    Create W1.m

---
+++ Workshop1/W1.m
@@ -0,0 +1,18 @@
+% WORKSHOP 1
+
+A = [-1 0 -1 4; 4 -1 0 -1; -1 4 -1 0; 0 -1 4 -1];
+b = [35; 25; 15; 25];
+
+disp("The solution using the inverse matrix method is: ")
+disp(inv(A)*b)
+
+[A_triu, b_triu] = gauss_solver(A, b);
+
+disp("The solution using the inverse matrix method with the Gaussian equivalent
+system is: ")
+disp(inv(A_triu)*b_triu)
+
+solution = upper_triangular_solver(A_triu, b_triu);
+
+disp("The solution solving the upper triangular system is: ")
+disp(solution)
